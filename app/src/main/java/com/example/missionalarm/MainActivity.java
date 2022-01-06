8bd836cd9eee3192379403e6941faaa935c7d042
Richet-Barton <options3224@gmail.com>
2022-01-06 12:56:10 +0900

    주석 추가 및 알람 추가/수정 이벤트 구분

--- app/src/main/java/com/example/missionalarm/MainActivity.java
+++ app/src/main/java/com/example/missionalarm/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.missionalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
@@ -10,39 +10,65 @@ import java.util.*;
 
 
 public class MainActivity extends AppCompatActivity {
+    List<String> list = new ArrayList<>();
+    ListView listView;
 
+    // 애플리케이션 초기 실행
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView tvSelect = findViewById(R.id.tv_select);
-        ListView listView = findViewById(R.id.listView);
+        final TextView tv = findViewById(R.id.textViewAlarm);
+        listView = findViewById(R.id.listView);
 
-        List<String> list = new ArrayList<>();
         list.add("06:00");
-        list.add("06:05");
-        list.add("06:10");
-        list.add("06:15");
-        list.add("06:20");
-        list.add("06:25");
-        list.add("06:30");
-        list.add("06:35");
-        list.add("06:40");
-        list.add("06:45");
-        list.add("06:50");
+        updateList();
 
-        System.out.println("Hello");
-
-        ArrayAdapter<String> adpater = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
-        listView.setAdapter(adpater);
-
+        // 리스트의 항목 클릭 시
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 String data = (String) adapterView.getItemAtPosition(position);
-                tvSelect.setText(data);
+                tv.setText(data);
+                setAlarm();
             }
         });
     }
+
+    // 알람 추가 버튼 클릭 시
+    public void addAlarm(View view) {
+        Intent intent = new Intent(this, SetAlarmActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    // 알람 수정
+    public void setAlarm() {
+        Intent intent = new Intent(this, SetAlarmActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    // startActivityForResult 이후 메인 화면으로 돌아올 때
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case 0: // 알람을 추가했을 때
+                if (resultCode == RESULT_OK)
+                    list.add("새로운 알람");
+                break;
+            case 1: // 알람을 수정했을 때
+                if (resultCode == RESULT_OK)
+                    list.add("수정된 알람");
+                break;
+        }
+        updateList();
+    }
+
+    // 리스트 새로고침
+    public void updateList() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+    }
+
 }
--- app/src/main/java/com/example/missionalarm/SetAlarmActivity.java
+++ app/src/main/java/com/example/missionalarm/SetAlarmActivity.java
@@ -3,12 +3,57 @@ package com.example.missionalarm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SetAlarmActivity extends AppCompatActivity {
+    List<String> list = new ArrayList<>();
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_alarm);
+
+        final TextView tvSelect = findViewById(R.id.textViewAlarm);
+        listView = findViewById(R.id.listView);
+
+        list.add("알람 이름");
+        list.add("시간");
+        list.add("요일");
+        list.add("알람음");
+        list.add("진동");
+        list.add("미션");
+        list.add("벌칙");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+        
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String data = (String) adapterView.getItemAtPosition(position);
+                tvSelect.setText(data);
+            }
+        });
     }
+
+    // 취소 버튼을 눌렀을 때
+    public void clickedButtonCancel(View view) {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+    // 선택 버튼을 눌렀을 때
+    public void clickedButtonAdd (View view) {
+        setResult(RESULT_OK);
+        finish();
+    }
+
 }
